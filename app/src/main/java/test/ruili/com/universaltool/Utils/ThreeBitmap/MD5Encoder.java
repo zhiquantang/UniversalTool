600fad59d112b5ad320d19ce4ff6331d9eff8f73
zhiquantang <zhiquan.tang@ruilicom.com>
2017-09-19 17:05:05 +0800

    [add] 添加三级图片缓存的工具类

---
+++ app/src/main/java/test/ruili/com/universaltool/Utils/ThreeBitmap/MD5Encoder.java
@@ -0,0 +1,29 @@
+package test.ruili.com.universaltool.Utils.ThreeBitmap;
+
+import java.security.MessageDigest;
+
+/**
+ * Created by Administrator on 2017/9/19 0019.
+ */
+
+public class MD5Encoder {
+    /**
+     * Md5Encoder
+     *
+     * @param string
+     * @return
+     * @throws Exception
+     */
+    public static String encode(String string) throws Exception {
+        byte[] hash = MessageDigest.getInstance("MD5").digest(
+                string.getBytes("UTF-8"));
+        StringBuilder hex = new StringBuilder(hash.length * 2);
+        for (byte b : hash) {
+            if ((b & 0xFF) < 0x10) {
+                hex.append("0");
+            }
+            hex.append(Integer.toHexString(b & 0xFF));
+        }
+        return hex.toString();
+    }
+}
---
+++ app/src/main/java/test/ruili/com/universaltool/Utils/ThreeBitmap/MyBitmapUtils.java
@@ -0,0 +1,47 @@
+package test.ruili.com.universaltool.Utils.ThreeBitmap;
+
+import android.graphics.Bitmap;
+import android.widget.ImageView;
+
+import test.ruili.com.universaltool.R;
+
+/**
+ * Created by Administrator on 2017/9/19 0019.
+ * BitmapUtils工具类,实现三级缓存
+ */
+public class MyBitmapUtils {
+
+    private NetCacheUtils mNetCacheUtils;
+    private LocalCacheUtils mLocalCacheUtils;
+    private MemoryCacheUtils mMemoryCacheUtils;
+
+    public MyBitmapUtils(){
+        mMemoryCacheUtils=new MemoryCacheUtils();
+        mLocalCacheUtils=new LocalCacheUtils();
+        mNetCacheUtils=new NetCacheUtils(mLocalCacheUtils,mMemoryCacheUtils);
+    }
+
+    public void disPlay(ImageView ivPic, String url) {
+        ivPic.setImageResource(R.mipmap.ic_launcher);
+        Bitmap bitmap;
+        //内存缓存
+        bitmap=mMemoryCacheUtils.getBitmapFromMemory(url);
+        if (bitmap!=null){
+            ivPic.setImageBitmap(bitmap);
+            System.out.println("从内存获取图片啦.....");
+            return;
+        }
+
+        //本地缓存
+        bitmap = mLocalCacheUtils.getBitmapFromLocal(url);
+        if(bitmap !=null){
+            ivPic.setImageBitmap(bitmap);
+            System.out.println("从本地获取图片啦.....");
+            //从本地获取图片后,保存至内存中
+            mMemoryCacheUtils.setBitmapToMemory(url,bitmap);
+            return;
+        }
+        //网络缓存
+        mNetCacheUtils.getBitmapFromNet(ivPic,url);
+    }
+}
